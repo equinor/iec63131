04a6b4d5281cecbdeb3d00320069751590a31f0d
AlexTxen <89920801+AlexTxen@users.noreply.github.com>
2023-05-31 23:00:05 +0200

    Change 'Krafla' to 'Yggdrasil'

---
+++ CHANGELOG-YGGDRASIL.md
@@ -0,0 +1,12 @@
+# Changelog Yggdrasil
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## 0.0.1 - 2023-06-01
+
+### Added
+
+- AML library 'YggdrasilAmlLibrary' as XML merge of iec63131 AML library v0.0.10 ('main') and 'YggdrasilPartialLibrary'
--- helper-scripts/CombineYggdrasilLibrary.ps1
+++ helper-scripts/CombineYggdrasilLibrary.ps1
@@ -1,13 +1,13 @@
 <#  
 .SYNOPSIS
-Combines IEC AML library and partial Krafla AML library into full Krafla AML library
+Combines IEC AML library and partial Yggdrasil AML library into full Yggdrasil AML library
 .DESCRIPTION
-1. Replaces IEC library version identifier by Krafla library identifier
-2. Combines contents of KraflaPartialLibrary.aml and NorsokSCDLibrary.aml
-3. Saves resulting AML to KraflaAMLLibrary.aml
+1. Replaces IEC library version identifier by Yggdrasil library identifier
+2. Combines contents of YggdrasilPartialLibrary.aml and NorsokSCDLibrary.aml
+3. Saves resulting AML to YggdrasilAMLLibrary.aml
 .NOTES
 Made based on AML version 2.1
-..\KraflaAMLLibrary.aml (with any content) shall exist in file system prior running script
+..\YggdrasilAMLLibrary.aml (with any content) shall exist in file system prior running script
 #>
 #Requires -Version 5.1
 
@@ -15,8 +15,8 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
 $iecFileName = "..\NorsokSCDLibrary.aml"
-$partialKraflaFileName = "..\KraflaPartialLibrary.aml"
-$resultFileName = "..\KraflaAMLLibrary.aml"
+$partialYggdrasilFileName = "..\YggdrasilPartialLibrary.aml"
+$resultFileName = "..\YggdrasilAMLLibrary.aml"
 
 function Add-MissingNode {
     param (
@@ -48,8 +48,8 @@ function Add-MissingNode {
 $iecFilePath = Resolve-Path $iecFileName
 [xml]$resultContent = Get-Content $iecFilePath.Path -Encoding utf8
 
-$partialKraflaFilePath = Resolve-Path $partialKraflaFileName
-[xml]$partialKraflaContent = Get-Content $partialKraflaFilePath.Path -Encoding utf8
+$partialYggdrasilFilePath = Resolve-Path $partialYggdrasilFileName
+[xml]$partialYggdrasilContent = Get-Content $partialYggdrasilFilePath.Path -Encoding utf8
 
 $resultFilePath = Resolve-Path $resultFileName
 
@@ -57,15 +57,15 @@ $ns = New-Object System.Xml.XmlNamespaceManager($resultContent.NameTable)
 $ns.AddNamespace("ns", "http://www.dke.de/CAEX")
 
 $libraryIecVersionNode = $resultContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
-$libraryKraflaVersionNode = $partialKraflaContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
+$libraryYggdrasilVersionNode = $partialYggdrasilContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
 
-$importedLibraryVersionNode = $resultContent.ImportNode($libraryKraflaVersionNode, $true)
+$importedLibraryVersionNode = $resultContent.ImportNode($libraryYggdrasilVersionNode, $true)
 $libraryIecVersionNode.ParentNode.AppendChild($importedLibraryVersionNode)
 Write-Host "Added node $($importedLibraryVersionNode.OuterXml)"
 $libraryIecVersionNode.ParentNode.RemoveChild($libraryIecVersionNode)
 Write-Host "Removed node $($libraryIecVersionNode.OuterXml)"
 
-Add-MissingNode -SourceNode $partialKraflaContent.CAEXfile -DestinationNode $resultContent.CAEXfile -DocumentToUpdate $resultContent -Namespace $ns
+Add-MissingNode -SourceNode $partialYggdrasilContent.CAEXfile -DestinationNode $resultContent.CAEXfile -DocumentToUpdate $resultContent -Namespace $ns
 
 $resultContent.Save($resultFilePath)
 
