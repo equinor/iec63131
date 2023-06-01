39b0d06338ab1113bbb37e2c18fb1c171ff586db
AlexTxen <89920801+AlexTxen@users.noreply.github.com>
2023-06-01 05:36:54 +0200

    Change combining script
    Script for main library and partial library combining was updated to preserve vendor sections of library

--- CHANGELOG-YGGDRASIL.md
+++ CHANGELOG-YGGDRASIL.md
@@ -10,3 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - AML library 'YggdrasilAmlLibrary' as XML merge of iec63131 AML library v0.0.10 ('main') and 'YggdrasilPartialLibrary'
+
+### Changed
+- 'CombineYggdrasilLibrary.ps1' script updated to preserve vendor sections
+- README-YGGDRASIL updated to allow editing vendor sections of YggdrasilAmlLibrary
--- helper-scripts/CombineYggdrasilLibrary.ps1
+++ helper-scripts/CombineYggdrasilLibrary.ps1
@@ -4,10 +4,11 @@ Combines IEC AML library and partial Yggdrasil AML library into full Yggdrasil A
 .DESCRIPTION
 1. Replaces IEC library version identifier by Yggdrasil library identifier
 2. Combines contents of YggdrasilPartialLibrary.aml and NorsokSCDLibrary.aml
-3. Saves resulting AML to YggdrasilAMLLibrary.aml
+3. Restores vendor sections from existing YggdrasilAMLLibrary.aml
+4. Saves resulting AML to YggdrasilAMLLibrary.aml
 .NOTES
 Made based on AML version 2.1
-..\YggdrasilAMLLibrary.aml (with any content) shall exist in file system prior running script
+..\YggdrasilAMLLibrary.aml shall exist in file system prior running script
 #>
 #Requires -Version 5.1
 
@@ -17,6 +18,12 @@ Set-StrictMode -Version Latest
 $iecFileName = "..\NorsokSCDLibrary.aml"
 $partialYggdrasilFileName = "..\YggdrasilPartialLibrary.aml"
 $resultFileName = "..\YggdrasilAMLLibrary.aml"
+
+$vendorSectionPaths = @(
+    "/ns:CAEXFile/ns:InterfaceClassLib[@Name='InterfaceClassLibrary']/ns:InterfaceClass[@Name='VendorSignalClass']"
+    "/ns:CAEXFile/ns:SystemUnitClassLib[@Name='FunctionBlockLibrary']/ns:SystemUnitClass[@Name='VendorFunctionBlockClass']"
+    "/ns:CAEXFile/ns:SystemUnitClassLib[@Name='ElementaryFunctionLibrary']/ns:SystemUnitClass[@Name='VendorElementaryFunctionClass']"
+)
 
 function Add-MissingNode {
     param (
@@ -29,21 +36,48 @@ function Add-MissingNode {
     foreach($node in $SourceNode.ChildNodes) {
 
         if(($node.NodeType -eq [System.Xml.XmlNodeType]::Element) -and ($node.get_name() -ne "AdditionalInformation")) {         
-            $path = "//ns:$($node.get_name())[@Name='$($node.Name)']"
-            #Write-Host $path
+            $path = "descendant::ns:$($node.get_name())[@Name='$($node.Name)']"
+            
             $existingNode = $DestinationNode.SelectSingleNode($path, $Namespace)
 
             if ($existingNode) {
                 Add-MissingNode -SourceNode $node -DestinationNode $existingNode $DocumentToUpdate $Namespace
             } else {
+                #Write-Host $path
                 $importedNode = $DocumentToUpdate.ImportNode($node, $true)
-                $DestinationNode.AppendChild($importedNode)
+                [void]$DestinationNode.AppendChild($importedNode)
                 Write-Host "Added node <$($importedNode.get_name()) Name='$($importedNode.Name)'>"
             }
         }       
     }
 }
 
+function Restore-VendorNode {
+    param (
+        [xml]$Source,
+        [xml]$Destination,
+        $Namespace
+    )
+
+    foreach($vendorSectionPath in $vendorSectionPaths) {
+        $vendorSectionSourceNode = $Source.SelectSingleNode($vendorSectionPath, $Namespace)
+        $vendorSectionDestinationNode = $Destination.SelectSingleNode($vendorSectionPath, $Namespace)
+
+        foreach($node in $vendorSectionSourceNode.ChildNodes) {
+             if (($node.NodeType -eq [System.Xml.XmlNodeType]::Element) -and ($node.get_name() -in @('SystemUnitClass', 'InterfaceClass'))) {
+                $path = "descendant::ns:$($node.get_name())[@Name='$($node.Name)']"
+                $existingNode = $vendorSectionDestinationNode.SelectSingleNode($path, $Namespace)
+                if ($existingNode) {                  
+                    [void]$vendorSectionDestinationNode.RemoveChild($existingNode)
+                    Write-Host "Removed node <$($existingNode.get_name()) Name='$($existingNode.Name)'> from $($vendorSectionDestinationNode.ParentNode.Name)"
+                }
+                $importedNode = $Destination.ImportNode($node, $true)
+                [void]$vendorSectionDestinationNode.AppendChild($importedNode)
+                Write-Host "Added node <$($importedNode.get_name()) Name='$($importedNode.Name)'> to $($vendorSectionDestinationNode.ParentNode.Name)"
+             }
+        }
+    }   
+}
 
 $iecFilePath = Resolve-Path $iecFileName
 [xml]$resultContent = Get-Content $iecFilePath.Path -Encoding utf8
@@ -51,7 +85,8 @@ $iecFilePath = Resolve-Path $iecFileName
 $partialYggdrasilFilePath = Resolve-Path $partialYggdrasilFileName
 [xml]$partialYggdrasilContent = Get-Content $partialYggdrasilFilePath.Path -Encoding utf8
 
-$resultFilePath = Resolve-Path $resultFileName
+$yggdrasilFilePath = Resolve-Path $resultFileName
+[xml]$existingYggdrasilContent = Get-Content $yggdrasilFilePath.Path -Encoding utf8
 
 $ns = New-Object System.Xml.XmlNamespaceManager($resultContent.NameTable)
 $ns.AddNamespace("ns", "http://www.dke.de/CAEX")
@@ -60,13 +95,17 @@ $libraryIecVersionNode = $resultContent.SelectSingleNode("/ns:CAEXFile/ns:Additi
 $libraryYggdrasilVersionNode = $partialYggdrasilContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
 
 $importedLibraryVersionNode = $resultContent.ImportNode($libraryYggdrasilVersionNode, $true)
-$libraryIecVersionNode.ParentNode.AppendChild($importedLibraryVersionNode)
+[void]$libraryIecVersionNode.ParentNode.AppendChild($importedLibraryVersionNode)
 Write-Host "Added node $($importedLibraryVersionNode.OuterXml)"
-$libraryIecVersionNode.ParentNode.RemoveChild($libraryIecVersionNode)
+[void]$libraryIecVersionNode.ParentNode.RemoveChild($libraryIecVersionNode)
 Write-Host "Removed node $($libraryIecVersionNode.OuterXml)"
 
+Write-Host "Adding nodes from partial library:"
 Add-MissingNode -SourceNode $partialYggdrasilContent.CAEXfile -DestinationNode $resultContent.CAEXfile -DocumentToUpdate $resultContent -Namespace $ns
 
-$resultContent.Save($resultFilePath)
+Write-Host "Restoring vendor nodes:"
+Restore-VendorNode -Source $existingYggdrasilContent -Destination $resultContent -Namespace $ns
 
+$resultContent.Save($yggdrasilFilePath)
+Write-Host 'Done'
 Start-Sleep -Seconds 10
