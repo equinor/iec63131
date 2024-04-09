4039ab80ba6cf236fd1cd9e3402c582f853dc7e4
AlexTxen <alex.txen@midt.no>
2024-04-09 17:11:49 +0200

    Init Snohvit library
    Initialiazation of Snohvit branch
    Added:
    - project readme
    - project changelog
    - project library
    - project scripts

--- helper-scripts/CombineSnohvitLibrary.ps1
+++ helper-scripts/CombineSnohvitLibrary.ps1
@@ -1,13 +1,13 @@
 <#  
 .SYNOPSIS
-Combines IEC AML library and partial Krafla AML library into full Krafla AML library
+Combines IEC AML library and partial Snohvit AML library into full Snohvit AML library
 .DESCRIPTION
-1. Replaces IEC library version identifier by Krafla library identifier
-2. Combines contents of KraflaPartialLibrary.aml and NorsokSCDLibrary.aml
-3. Saves resulting AML to KraflaAMLLibrary.aml
+1. Replaces IEC library version identifier by Snohvit library identifier
+2. Combines contents of SnohvitPartialLibrary.aml and NorsokSCDLibrary.aml
+3. Saves resulting AML to SnohvitAMLLibrary.aml
 .NOTES
 Made based on AML version 2.1
-..\KraflaAMLLibrary.aml (with any content) shall exist in file system prior running script
+..\SnohvitAMLLibrary.aml (with any content) shall exist in file system prior running script
 #>
 #Requires -Version 5.1
 
@@ -15,8 +15,8 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
 $iecFileName = "..\NorsokSCDLibrary.aml"
-$partialKraflaFileName = "..\KraflaPartialLibrary.aml"
-$resultFileName = "..\KraflaAMLLibrary.aml"
+$partialSnohvitFileName = "..\SnohvitPartialLibrary.aml"
+$resultFileName = "..\SnohvitAMLLibrary.aml"
 
 function Add-MissingNode {
     param (
@@ -48,8 +48,8 @@ function Add-MissingNode {
 $iecFilePath = Resolve-Path $iecFileName
 [xml]$resultContent = Get-Content $iecFilePath.Path -Encoding utf8
 
-$partialKraflaFilePath = Resolve-Path $partialKraflaFileName
-[xml]$partialKraflaContent = Get-Content $partialKraflaFilePath.Path -Encoding utf8
+$partialSnohvitFilePath = Resolve-Path $partialSnohvitFileName
+[xml]$partialSnohvitContent = Get-Content $partialSnohvitFilePath.Path -Encoding utf8
 
 $resultFilePath = Resolve-Path $resultFileName
 
@@ -57,15 +57,15 @@ $ns = New-Object System.Xml.XmlNamespaceManager($resultContent.NameTable)
 $ns.AddNamespace("ns", "http://www.dke.de/CAEX")
 
 $libraryIecVersionNode = $resultContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
-$libraryKraflaVersionNode = $partialKraflaContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
+$librarySnohvitVersionNode = $partialSnohvitContent.SelectSingleNode("/ns:CAEXFile/ns:AdditionalInformation/ns:Document", $ns)
 
-$importedLibraryVersionNode = $resultContent.ImportNode($libraryKraflaVersionNode, $true)
+$importedLibraryVersionNode = $resultContent.ImportNode($librarySnohvitVersionNode, $true)
 $libraryIecVersionNode.ParentNode.AppendChild($importedLibraryVersionNode)
 Write-Host "Added node $($importedLibraryVersionNode.OuterXml)"
 $libraryIecVersionNode.ParentNode.RemoveChild($libraryIecVersionNode)
 Write-Host "Removed node $($libraryIecVersionNode.OuterXml)"
 
-Add-MissingNode -SourceNode $partialKraflaContent.CAEXfile -DestinationNode $resultContent.CAEXfile -DocumentToUpdate $resultContent -Namespace $ns
+Add-MissingNode -SourceNode $partialSnohvitContent.CAEXfile -DestinationNode $resultContent.CAEXfile -DocumentToUpdate $resultContent -Namespace $ns
 
 $resultContent.Save($resultFilePath)
 
---
+++ helper-scripts/RemoveWriterHeadersFromSnohvitAmlFile.ps1
@@ -0,0 +1,42 @@
+<#  
+.SYNOPSIS
+Strips SourceDocumentInformation nodes from .aml file for cleaner version history
+.DESCRIPTION
+Purpose: Eliminate non-relevant content from IEC 63131 library version control
+.NOTES
+Made based on AML version 2.10 (CAEX 3)
+#>
+#Requires -Version 5.1
+
+# param(
+#   # The aml file name
+#   [Parameter(Mandatory)]
+#   [ValidateNotNullOrEmpty()]
+#   [string]$AmlFileName
+# )
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+# if (-not (Test-Path $AmlFileName -Leaf)) {
+#     Write-Error "File not found"
+#     exit
+# }
+#$amlFile = Resolve-Path $AmlFileName
+
+$amlFileName = "..\SnohvitlAMLLibrary.aml"
+$amlFilePath = Resolve-Path $amlFileName
+
+[xml]$amlContent = Get-Content $amlFilePath.Path -Encoding utf8
+
+$sourceDocumentInformationElements = Select-Xml -Xml $amlContent -XPath "/ns:CAEXFile/ns:SourceDocumentInformation" -Namespace @{ns="http://www.dke.de/CAEX"}
+
+$removedSourceDocumentInformationElementsCount = 0
+foreach ($sourceDocumentInformationElement in $sourceDocumentInformationElements) {
+    $amlContent.CAEXFile.RemoveChild($sourceDocumentInformationElement.Node)
+    $removedSourceDocumentInformationElementsCount++
+}
+
+$amlContent.Save($amlFilePath.Path)
+Write-Host "Removed $removedSourceDocumentInformationElementsCount SourceDocumentInformation elements."
+Start-Sleep -Seconds 2
